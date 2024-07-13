568306c402a82fba882c2f28682e0dac93194484
Zhuravleva0701 <167981362+Zhuravleva0701@users.noreply.github.com>
2024-07-13 22:59:22 +0300

    Add files via upload

--- homework7_9.py
+++ homework7_9.py
@@ -1,16 +1,32 @@
+def natural(number):
+    if not isinstance(number, int) or number <= 0:
+        raise TypeError
+
+
 def is_prime(func):
     def wrapper(*args, **kwargs):
         value = func(*args, **kwargs)
-        result = 'Простое' if isinstance(value, int) else 'Сложное'
-
-        return f"{value} это {result}"
+        natural(value)
+        x = 2
+        while x < value:
+            if value % x != 0:
+                x += 1
+            else:
+                print('Сложное')
+                return value
+        print('Простое')
+        return value
 
     return wrapper
 
 
-@is_prime
-def sum_three(*numbers):
-    return sum(numbers)
+try:
+    @is_prime
+    def sum_three(*numbers):
+        return sum(numbers)
 
 
-print(sum_three(2, 3, 6))
+    print(sum_three(2, 3, 6))
+
+except TypeError:
+    print('Это не натуральное число')
